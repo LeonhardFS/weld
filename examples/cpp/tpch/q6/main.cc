5b3c39afecb2b50b9221eb86fe6b0993ec3da942
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2020-09-19 22:40:34 -0400

    finally something working

--- examples/cpp/tpch/q6/main.cc
+++ examples/cpp/tpch/q6/main.cc
@@ -3,7 +3,12 @@
 //
 
 #include <iostream>
+#include <string>
 #include "../../../../weld-capi/weld.h"
+
+#include "CLI11.hpp"
+#include "csvmonkey.h"
+#include "timer.h"
 using namespace std;
 
 // @TODO: need to load FULL data, so it's fair to the other frameworks...
@@ -13,7 +18,7 @@ struct weld_vector {
     int64_t length;
 };
 
-struct args {
+struct Lineitem {
     struct weld_vector<int32_t> shipdates;
     struct weld_vector<double> discounts;
     struct weld_vector<double> quantities;
@@ -42,10 +47,72 @@ const char* program = "|l_shipdate: vec[i32], l_discount: vec[f64], l_quantity: 
                       "    ))\n"
                       "";
 
-int main(int argc, char* argv[]) {
-    cout<<"Hello world"<<endl;
 
+// all the (required) vectors
+vector<int32_t> shipdates;
+vector<double> discounts;
+vector<double> quantities;
+vector<double> extended_prices;
 
+//         TableDefinition.Column("l_orderkey", SqlType.big_int(), NOT_NULLABLE),  0
+//        TableDefinition.Column("l_partkey", SqlType.big_int(), NOT_NULLABLE),    1
+//        TableDefinition.Column("l_suppkey", SqlType.big_int(), NOT_NULLABLE),     2
+//        TableDefinition.Column("l_linenumber", SqlType.big_int(), NOT_NULLABLE),  3
+//        TableDefinition.Column("l_quantity", SqlType.double(), NOT_NULLABLE),     4
+//        TableDefinition.Column("l_extendedprice", SqlType.double(), NOT_NULLABLE),5
+//        TableDefinition.Column("l_discount", SqlType.double(), NOT_NULLABLE),     6
+//        TableDefinition.Column("l_tax", SqlType.double(), NOT_NULLABLE),          7
+//        TableDefinition.Column("l_returnflag", SqlType.char(1), NOT_NULLABLE),    8
+//        TableDefinition.Column("l_linestatus", SqlType.char(1), NOT_NULLABLE),    9
+//        TableDefinition.Column("l_shipdate", SqlType.date(), NOT_NULLABLE),       10
+//        TableDefinition.Column("l_commitdate", SqlType.date(), NOT_NULLABLE),     11
+//        TableDefinition.Column("l_receiptdate", SqlType.date(), NOT_NULLABLE),    12
+//        TableDefinition.Column("l_shipinstruct", SqlType.char(25), NOT_NULLABLE),
+//        TableDefinition.Column("l_shipmode", SqlType.char(10), NOT_NULLABLE),
+//        TableDefinition.Column("l_comment", SqlType.varchar(44), NOT_NULLABLE)
+static const int SHIPDATE_IDX = 10;
+static const int DISCOUNT_IDX = 6;
+static const int QUANTITY_IDX = 4;
+static const int EXTENDED_PRICE_IDX = 5;
+
+int main(int argc, char** argv) {
+
+    // cli11 quick parse for file, cf. https://cliutils.gitlab.io/CLI11Tutorial/
+    CLI::App app("Weld TPC-H Q6");
+
+    string data_path = "";
+    app.add_option("-p,--path", data_path, "lineitem.tbl data path")
+    ->required()
+    ->check(CLI::ExistingFile);
+
+    CLI11_PARSE(app, argc, argv);
+
+    cout<<"data path is: "<<data_path<<endl;
+
+    Timer timer;
+    // parse file
+    auto fd = open(data_path.c_str(), O_RDONLY);
+    csvmonkey::FdStreamCursor stream(fd);
+    csvmonkey::CsvReader<csvmonkey::FdStreamCursor> reader(stream, '|');
+
+    while(reader.read_row()) {
+        auto& cursor = reader.row();
+        auto raw_date = cursor.cells[SHIPDATE_IDX].as_str();
+        auto date_str = raw_date.substr(0, 4) + raw_date.substr(5, 2) + raw_date.substr(8, 2);
+        shipdates.emplace_back(stoi(date_str));
+
+        // parse the others
+        discounts.emplace_back(cursor.cells[DISCOUNT_IDX].as_double());
+        quantities.emplace_back(cursor.cells[QUANTITY_IDX].as_double());
+        extended_prices.emplace_back(cursor.cells[EXTENDED_PRICE_IDX].as_double());
+    }
+
+    close(fd);
+    double load_time = timer.time();
+    cout<<"Loading CSV took: "<<load_time<<"s"<<endl;
+    timer.reset();
+
+    // convert to Weld
     weld_error_t  e = weld_error_new();
     weld_conf_t conf = weld_conf_new();
 
@@ -57,6 +124,50 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    double compile_time = timer.time();
+    cout<<"Compiling the Weld module took: "<<compile_time<<"s"<<endl;
+    timer.reset();
 
+    auto num_rows = shipdates.size();
+    Lineitem tbl;
+    tbl.shipdates = make_weld_vector<int32_t>(shipdates.data(), num_rows);
+    tbl.discounts = make_weld_vector<double>(discounts.data(), num_rows);
+    tbl.quantities = make_weld_vector<double>(quantities.data(), num_rows);
+    tbl.extended_prices = make_weld_vector<double>(extended_prices.data(), num_rows);
+
+    weld_value_t weld_lineitem = weld_value_new(&tbl);
+
+    conf = weld_conf_new();
+    auto ctx = weld_context_new(conf);
+    weld_value_t result = weld_module_run(m, ctx, weld_lineitem, e);
+    if(weld_error_code(e)) {
+        const char *err = weld_error_message(e);
+        cerr<<"Error message: "<<err<<endl;
+        exit(1);
+    }
+
+    auto* result_data = (double*)weld_value_data(result);
+    double final_result = *result_data;
+
+    cout<<fixed<<"Result::\n"<<final_result<<endl;
+
+    // Free the values.
+    weld_value_free(result);
+    weld_value_free(weld_lineitem);
+    weld_context_free(ctx);
+    weld_conf_free(conf);
+
+    weld_error_free(e);
+    weld_module_free(m);
+
+    double query_time = timer.time();
+    cout<<"Weld query took: "<<query_time<<"s"<<endl;
+
+    cout<<endl;
+
+    // nice parseable representation of the numbers
+    cout << fixed << "framework,load,compile,query,total\n"
+         << "weld," << load_time << "," << compile_time << "," << query_time << ","
+         << load_time + compile_time + query_time << endl;
     return 0;
 }
